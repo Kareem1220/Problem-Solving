b58711d9bdfeeeefe6c1fcaeb01256a95f4758c2
Kareem Hamza <1220708@student.birzeit.edu>
2026-09-03 13:30:12 +0300

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.cpp
@@ -0,0 +1,57 @@
+class PrefixTree 
+{
+public:
+    struct TrieNode 
+    {
+        TrieNode* children[26];
+        bool isEnd;
+        TrieNode() 
+        {
+            for(int i = 0; i < 26; i++) children[i] = nullptr;
+            isEnd = false;
+        }
+    };
+    TrieNode* trie;
+
+    PrefixTree() 
+    {
+        trie = new TrieNode();
+    }
+    void insert(string word) // fix the unnecessary second if statement
+    {
+        TrieNode* current = trie;
+        for(int i = 0 ; i < word.size() ; i++)
+        {
+            if(current->children[word[i] - 'a'] == nullptr)
+            {
+                TrieNode* branch = new TrieNode();
+                current->children[word[i] - 'a'] = branch;
+            }
+            current = current->children[word[i] - 'a'];
+            if(i == word.size() - 1) current->isEnd = true;
+        }
+    }
+    
+    bool search(string word) // the return value after the loop could be cleaner next time
+    {
+        TrieNode* current = trie;
+        for(int i = 0 ; i < word.size() ; i++)
+        {
+            if(current->children[word[i] - 'a'] == nullptr) return false;
+            current = current->children[word[i] - 'a'];
+        }
+        if(current->isEnd == true) return true;
+        else return false;
+    }
+
+    bool startsWith(string prefix) 
+    {
+        TrieNode* current = trie;
+        for(int i = 0 ; i < prefix.size() ; i++)
+        {
+            if(current->children[prefix[i] - 'a'] == nullptr) return false;
+            current = current->children[prefix[i] - 'a'];
+        }
+        return true;
+    }
+};
