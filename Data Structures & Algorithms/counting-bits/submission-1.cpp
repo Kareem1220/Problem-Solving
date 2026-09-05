ad07e5d3af76e5022daa7828b5911ffe8a5a2be4
Kareem Hamza <1220708@student.birzeit.edu>
2026-09-05 16:44:55 +0300

    Add: counting-bits - submission-1

---
+++ Data Structures & Algorithms/counting-bits/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> countBits(int n) 
+    {
+        vector<int> sol(n + 1);
+        for(int i = 1; i <= n; i++)
+        {
+            sol[i] = sol[i / 2] + (i % 2);
+        }
+        return sol;
+    }
+};
