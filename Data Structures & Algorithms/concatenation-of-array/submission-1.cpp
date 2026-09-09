11c62f170cbc4cbcaef89ff8a5ffab306f0b245c
Kareem Hamza <1220708@student.birzeit.edu>
2026-09-09 19:34:15 +0300

    Add: concatenation-of-array - submission-1

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-1.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) 
+    {
+        vector<int> ans;
+        ans.reserve(nums.size() * 2);
+        for(int i = 0 ; i < nums.size() ; i++) ans.push_back(nums[i]);
+        for(int i = 0 ; i < nums.size() ; i++) ans.push_back(nums[i]);
+        return ans;
+    }
+};
