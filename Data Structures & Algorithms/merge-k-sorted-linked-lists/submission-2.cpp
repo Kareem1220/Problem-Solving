1cbd1747b346eaad5693ea93497e2c24245c2bb3
Kareem Hamza <1220708@student.birzeit.edu>
2026-09-14 17:24:41 +0300

    Add: merge-k-sorted-linked-lists - submission-2

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-2.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode 
+ {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution 
+{
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) 
+    {
+        for(int i = 0 ; i < lists.size() ; i++)
+        {
+            if(lists[i] == nullptr) 
+            {
+                lists.erase(lists.begin() + i);
+                i--;
+            }
+        }
+        if(lists.empty()) return nullptr;
+        ListNode* h = lists[0];
+        for(int i = 1 ; i < lists.size() ; i++)
+        {
+            while(h->next != nullptr) 
+            {
+                h = h->next;
+            }
+            h->next = lists[i];
+        }
+        ListNode* t = lists[0];
+        return sortList(t);
+    }
+    ListNode* sortList(ListNode* t)
+    {
+        if(!t || !t->next) return t;
+        ListNode* slow = t;
+        ListNode* fast = t->next;
+        while(fast != nullptr && fast->next != nullptr)
+        {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+        ListNode* rightList = slow->next;
+        slow->next = nullptr;
+
+        ListNode* left = sortList(t);
+        ListNode* right = sortList(rightList);
+
+        return merge(left,right);
+    }
+    ListNode* merge(ListNode* l1 , ListNode* l2)
+    {
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+        while(l1 != nullptr && l2 != nullptr)
+        {
+            if(l1->val <= l2->val)
+            {
+                tail->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
